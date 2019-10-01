093873720b55431acd238f63a110a0c65386f680
Geoffrey Neal <geoffrey_neal@hotmail.com>
2019-10-01 22:41:10 +0100

    CurrencyBox component created. Preliminary styles completed.

--- src/Components/Converter/index.js
+++ src/Components/Converter/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Converter.css';
 
+// Components
+import CurrencyBox from '../CurrencyBox';
+
+// Assets
+import exchangeIcon from './exchange-arrows.svg';
+
 const Converter = () => {
   return (
     <div className="Converter">
-      <h1>Currency Converter</h1>
+      <h1 className="Converter__header">Currency Converter</h1>
+      <div className="Converter__input-container">
+        <CurrencyBox />
+        <img src={exchangeIcon} className="Converter__exchange-button" />
+        <CurrencyBox />
+      </div>
     </div>
   );
 };
